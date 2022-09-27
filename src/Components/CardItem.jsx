b2954b481237f3f32e86f853a0c8e08a97ed39e7
JATIN <bishtjatinsingh@gmail.com>
2022-09-27 22:28:13 +0530

    first commit

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row';
 import { BsFillHeartFill } from "react-icons/bs"
 import { v4 as uuid } from 'uuid';
 import Banner from './Banner';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const CardItem = ({ topSongs }) => {
     const [fav, setFav] = useState(JSON.parse(localStorage.getItem("fav") || "[]"))
@@ -16,7 +18,7 @@ const CardItem = ({ topSongs }) => {
         let present = JSON.parse(localStorage.getItem("fav")).filter((item) => {
             return item.title === favs
         })
-       
+
         if (!(present.length === 0)) {
             alert("Already Added To Favourite List")
             return
@@ -24,27 +26,44 @@ const CardItem = ({ topSongs }) => {
             setFav(prev => [...prev, { title: favs, img: image, id: uuid() }]);
             alert("SUCCESSFULLY ADDED TO THE PLAYLIST..")
         }
-   }
+    }
+
+
+    const responsive = {
+        0: { items: 1 },
+        450: { items: 2 },
+        568: { items: 3 },
+        1024: { items: 5 },
+    };
+
+
+    const items = topSongs.map((item, index) => {
+        return <Card className=" mx-3 my-5  cardHover position-relative overflow-hidden" key={index}>
+            <Card.Img variant="top" src={item.images.coverart} />
+            <Card.Body>
+                <Card.Title className="text-center">{item.title}</Card.Title>
+            </Card.Body>
+            <div className="overlay fs-1"><BsFillHeartFill className="icon" onClick={() => addToFav(`${item.title}`, `${item.images.coverart}`)} /></div>
+        </Card>
+    })
 
     return (
-        
-            <div className="container-fluid">
-               <Banner>
+
+        <div className="container-fluid"
+            style={{ minHeight: "100vh" }}>
+            <Banner>
                 <h1>HOME</h1>
-               </Banner>
-                <Row className="d-flex justify-content-center">  
-                    {topSongs.map((item, index) => {
-                    return <Card className="col-sm-12 col-md-4 col-lg-3 m-3  cardHover position-relative overflow-hidden" key={index}>
-                        <Card.Img variant="top" src={item.images.coverart} />
-                        <Card.Body>
-                            <Card.Title className="text-center">{item.title}</Card.Title>
-                        </Card.Body>
-                        <div className="overlay fs-1"><BsFillHeartFill className="icon" onClick={() => addToFav(`${item.title}`, `${item.images.coverart}`)} /></div>
-                    </Card>
-                })}
-                </Row>
-            </div>
-        
+            </Banner>
+
+            <Row className="d-flex justify-content-center">  <AliceCarousel
+                mouseTracking
+                items={items}
+                responsive={responsive}
+                controlsStrategy="alternate" />
+            </Row>
+
+        </div>
+
     )
 }
 
